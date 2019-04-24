ae3361efbc68782ea9be1981bc888c499a6f00b0
andy <787187810@qq.com>
2019-04-24 17:31:42 +0800

    init

---
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.firstapp
+
+import android.media.MediaPlayer
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var mediaPlayer: MediaPlayer
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        mediaPlayer = MediaPlayer.create(this, R.raw.song)
+        mediaPlayer.prepareAsync()
+        mediaPlayer.start()
+//        findViewById<Button>(R.id.start).setOnClickListener {
+//            mediaPlayer.start()
+//        }
+//        findViewById<Button>(R.id.pause).setOnClickListener {
+//            mediaPlayer.pause()
+//        }
+//        findViewById<Button>(R.id.stop).setOnClickListener {
+//            mediaPlayer.stop()
+//        }
+
+    }
+
+    fun onClickPause(view: View) {
+        mediaPlayer.pause()
+
+    }
+
+    fun onClickStart(view: View) {
+        if (mediaPlayer.isPlaying) {
+            mediaPlayer.prepare()
+        } else
+            mediaPlayer.start()
+    }
+
+    fun onClickStop(view: View) {
+        mediaPlayer.stop()
+    }
+
+}
